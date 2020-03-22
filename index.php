107f38aa5c89af324c30d3c435f6091b47e49de3
miishha <thatisnotthecase@gmail.com>
2020-03-22 22:37:18 +0300

    ABCD

--- index.php
+++ index.php
@@ -3,4 +3,5 @@ $GLOBALS['import'] = function($_) { return require($_); };
 #$GLOBALS['import']('app/static/.init');
 $app = $GLOBALS['import']('app/sys/app');
 $defined_vars = get_defined_vars();
+header ('content-type:application/json');
 print $app($defined_vars) ;
